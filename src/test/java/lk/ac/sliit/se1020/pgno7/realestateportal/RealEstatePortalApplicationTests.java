0e16e5cb32d627d99dbac2c15db0b10565929484
IT24100839 <IT24100839@my.sliit.lk>
2025-05-22 23:07:00 +0530

    Created Main & Test Java files for components

---
+++ src/test/java/lk/ac/sliit/se1020/pgno7/realestateportal/RealEstatePortalApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ac.sliit.se1020.pgno7.realestateportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstatePortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
